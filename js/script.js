afb0d8a176fab9a817ed4b80d47bc2e4e27796ec
JUNG-MIN WOO <jayxwoo@gmail.com>
2020-08-26 07:53:32 +0900

    [js] created answer checker object

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 // ========== imports ==========
 import './default.js';
 
+// reference
+const form = document.querySelector('.quiz-form');
+
+// answer checker
+let answerChecker = {
+    // properties
+    form: form,
+    answers: ['b', 'a', 'c', 'a'],
+
+    // methods
+    getUserAnswers: function () {
+        let checkedRadios = this.form.querySelectorAll('.input-radio:checked');
+        let userAnswers = [];
+        checkedRadios.forEach(function (checkedRadio) {
+            userAnswers.push(checkedRadio.value);
+        });
+        
+        return userAnswers;
+    },
+
+    checkUserAnswers: function (userAnswers) {
+        
+    }
+};
+
+// main
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const userAnswers = answerChecker.getUserAnswers();
+    answerChecker.checkUserAnswers(userAnswers);
+});
